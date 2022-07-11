f102f84b0bb163919b8e49ff5abeb4b072b89d87
Zigmund Sun Oo <zigmund.sunoo@credera.com>
2022-07-11 14:01:28 -0500

    [underline name] init

--- src/components/profile-header.js
+++ src/components/profile-header.js
@@ -1,8 +1,13 @@
+import underlineSrc from "../assets/underline.svg";
+
 export const updateProfileInformation = (data) => {
   const { firstName, lastName, avatarSrc } = data;
   const headerNode = document.querySelector("#profile-header .profile-header");
   const profileAvatarNode = headerNode.querySelector("img");
   const nameNode = headerNode.querySelector(".profile-info .profile-info-name");
+  const underlineNode = headerNode.querySelector(".profile-underline");
+
+  underlineNode.setAttribute("src", underlineSrc);
 
   nameNode.classList.remove(
     "loading",
@@ -11,6 +16,7 @@ export const updateProfileInformation = (data) => {
   );
 
   nameNode.innerHTML = `${firstName} ${lastName}`;
+  nameNode.appendChild(underlineNode);
   profileAvatarNode.src = avatarSrc;
   profileAvatarNode.setAttribute("aria-label", `${firstName} ${lastName}`);
 
